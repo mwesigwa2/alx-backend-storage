5be13af9e3a5f9a4e7806f1d97260e8312b45fea
mwesigwa2 <tmwesigwa2@gmail.com>
2024-01-25 02:27:45 +0000

    Reading from Redis and recovering original type

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -12,7 +12,7 @@ int or float.
 '''
 import uuid
 import redis
-from typing import Union
+from typing import Union, Callable, Optional
 
 
 class Cache:
@@ -30,3 +30,31 @@ class Cache:
 
         # Return the generated key
         return key
+
+    def get(self, key: str,
+            fn: Optional[callable] = None) -> Union[str, bytes, int, float]:
+        # convert the data back to the desired format
+        value = self._redis.get(key)
+        if fn:
+            value = fn(value)
+        return value
+
+    def get_str(self, key: str) -> str:
+        '''
+        automatically parametrize Cache.get with the correct conversion
+        function
+        '''
+        value = self._redis.get(key)
+        return value.decode("utf-8")
+
+    def get_int(self, key: str) -> int:
+        '''
+        automatically parametrize Cache.get with the correct conversion
+        function
+        '''
+        value = self._redis.get(key)
+        try:
+            value = int(value.decode("utf-8"))
+        except Exception:
+            value = 0
+        return value
